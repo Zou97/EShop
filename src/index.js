5dc0ee0305b3fe4f90c5637a7f7c734741923d84
Zou97 <44450432+Zou97@users.noreply.github.com>
2019-04-03 14:35:08 -0400

    Socket.io
    pruebas con el chat usando socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 
+
+// Herramientas
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
+
+// Servidor
+const server = app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
@@ -13,6 +21,22 @@ app.use(express.json());
 app.use(require('./rutas/index'));
 app.use('/api/productos',require('./rutas/productos'));
 
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+//Estaticos
+app.use(express.static(path.join(__dirname, 'Temporal')));
+
+//Socket
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('nueva conexion', socket.id);
+
+    socket.on('mimensaje', (data) => {
+        io.sockets.emit('mimensaje', data);
+    });
+
+    socket.on('chat:typing', (data) => {
+        socket.broadcast.emit('chat:typing', data);
+    });
 });
+
